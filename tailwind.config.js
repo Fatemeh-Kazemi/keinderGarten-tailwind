93b0198d4aabd3892a42230572a512ff0b7287e9
Ftme Kazemi <kazemifk484@gmail.com>
2024-10-12 18:13:53 +0330

    add header section

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/*.html"],
+  theme: {
+    extend: {},
+    colors:{
+      C1: "#F29F05",
+      C2: "#3CA642",
+      C3: "#735BA5",
+      C4: "#D93735",
+      Text1: "#333333",
+      Text2: "#828282"
+    },
+    fontFamily:{
+      vazirmatn_b: ["'vazirmatn-b'"],
+      vazirmatn_l: ["'vazirmatn-l'"]
+    }
+  },
+  plugins: [],
+}
+
